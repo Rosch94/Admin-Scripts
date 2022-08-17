1f17e76e19040de8ab2f4f26f59f25f7109b5a6d
Rosch94 <Roschabdolaziz@gmail.com>
2022-08-17 11:52:49 +0200

    Update CompareExcelStringsWithWebsiteStrings.py
    edit try catch because of 404 error responses

--- CompareExcelStringsWithWebsiteStrings.py
+++ CompareExcelStringsWithWebsiteStrings.py
@@ -1,14 +1,15 @@
 import urllib.parse
 import urllib.request
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 import time
 
 nein = "Nein"
 ja = "Ja"
+error = "404 Fehler"
 
-
-path="c:/test13.xlsx"
+path="c:/test23.xlsx"
 wb = load_workbook(path)
 ws=wb.active
 counter = 1
@@ -18,41 +19,46 @@ headers={'User-Agent':user_agent,}
 
 
 for col in ws['A']:
-    webpage = col.value
-    request= urllib.request.Request(webpage,None,headers) #The assembled request
-    response = urllib.request.urlopen(request)
-    data = response.read() # The data u need
-    output = data.decode('utf-8')
-    soup = BeautifulSoup(output, 'html.parser')
-    comparedata = soup.find_all('p')
-    titel = soup.title.string
-    
-    if "beratung" in str(comparedata):
-        ws.cell(row=counter, column=2).value = ja
-        ws.cell(row=counter, column=4).value = titel
-    elif "Beratung" in str(comparedata):
-        ws.cell(row=counter, column=2).value = ja
-        ws.cell(row=counter, column=4).value = titel  
-    elif "beraten" in str(comparedata):
-        ws.cell(row=counter, column=2).value = ja
-        ws.cell(row=counter, column=4).value = titel
-    elif "Beraten" in str(comparedata):
-        ws.cell(row=counter, column=2).value = ja
-        ws.cell(row=counter, column=4).value = titel
-    else:
-        ws.cell(row=counter, column=2).value = nein
 
-    if "consulting" in str(comparedata):
-        ws.cell(row=counter, column=3).value = ja
-        ws.cell(row=counter, column=4).value = titel
-    elif "Consulting" in str(comparedata):
-        ws.cell(row=counter, column=3).value = ja
-        ws.cell(row=counter, column=4).value = titel
-    else:
-         ws.cell(row=counter, column=3).value = nein
+    print(counter)   
+    try:
+        webpage = col.value
+        request= urllib.request.Request(webpage,None,headers) #The assembled request
+        response = urllib.request.urlopen(request)
+        data = response.read() # The data u need
+        output = data.decode('utf-8')
+        soup = BeautifulSoup(output, 'html.parser')
+        comparedata = soup.find_all('p')
+        titel = soup.title.string
+        if "beratung" in str(comparedata):
+            ws.cell(row=counter, column=2).value = ja
+            ws.cell(row=counter, column=4).value = titel
+        elif "Beratung" in str(comparedata):
+            ws.cell(row=counter, column=2).value = ja
+            ws.cell(row=counter, column=4).value = titel  
+        elif "beraten" in str(comparedata):
+            ws.cell(row=counter, column=2).value = ja
+            ws.cell(row=counter, column=4).value = titel
+        elif "Beraten" in str(comparedata):
+            ws.cell(row=counter, column=2).value = ja
+            ws.cell(row=counter, column=4).value = titel
+        else:
+            ws.cell(row=counter, column=2).value = nein
 
-    counter = counter + 1
-    time.sleep(10)
+        if "consulting" in str(comparedata):
+            ws.cell(row=counter, column=3).value = ja
+            ws.cell(row=counter, column=4).value = titel
+        elif "Consulting" in str(comparedata):
+            ws.cell(row=counter, column=3).value = ja
+            ws.cell(row=counter, column=4).value = titel
+        else:
+            ws.cell(row=counter, column=3).value = nein  
+        counter = counter + 1
+        #wb.save("c:/test17.xlsx")
+        time.sleep(2)            
+    except HTTPError as err: 
+        ws.cell(row=counter, column=4).value = error
+        counter = counter + 1
+        time.sleep(2)
 
-wb.save("c:/test13.xlsx")
-
+wb.save("c:/test23.xlsx")
